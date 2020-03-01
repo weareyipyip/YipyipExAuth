675e94d12ff896e5efc0bf18768b1b9fd0746575
J. Smeets <j.smeets@yipyip.nl>
2020-03-01 19:45:41 +0100

    Vanilla Elixir project

---
+++ lib/yipyip_ex_auth.ex
@@ -0,0 +1,18 @@
+defmodule YipyipExAuth do
+  @moduledoc """
+  Documentation for `YipyipExAuth`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> YipyipExAuth.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/yipyip_ex_auth_test.exs
@@ -0,0 +1,8 @@
+defmodule YipyipExAuthTest do
+  use ExUnit.Case
+  doctest YipyipExAuth
+
+  test "greets the world" do
+    assert YipyipExAuth.hello() == :world
+  end
+end
